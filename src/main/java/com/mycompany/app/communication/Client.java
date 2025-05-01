ae41634d31eede593345872716ec9ec81359983e
Gooseclemons <scottwilmot04@gmail.com>
2025-04-30 23:35:56 -0400

    Refactored Host.java for accepting multiple client connections. Added Client.java for later development. Cleared Main.java to prevent compilation errors.

---
+++ src/main/java/com/mycompany/app/communication/Client.java
@@ -0,0 +1,17 @@
+package com.mycompany.app.communication;
+
+public class Client {
+
+    Client() {
+
+    }
+
+    public void connect() {
+
+    }
+
+    public void send_update() {
+
+    }
+
+}
