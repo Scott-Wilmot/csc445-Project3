d89aea39a2bbd2879c0bb7b168d4497710e41efb
Gooseclemons <scottwilmot04@gmail.com>
2025-04-30 21:13:51 -0400

    Saurav client stuff

---
+++ src/main/java/com/mycompany/app/model/Client.java
@@ -0,0 +1,37 @@
+package com.mycompany.app.model;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class Client {
+
+    public static void main(String[] args) {
+        try {
+            String myIP = InetAddress.getLocalHost().getHostAddress();
+            System.out.println(myIP);
+            create(myIP);
+        } catch (Exception e) {
+
+        }
+    }
+
+    public static DatagramSocket create(String ip) {
+        try {
+            byte[] buf = "10101010".getBytes();
+            DatagramSocket socket = new DatagramSocket(8080);
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, InetAddress.getByName("129.3.171.203"), 8080);
+            socket.send(packet);
+            System.out.println("Hi Saurav");
+            socket.receive(packet);
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+}
