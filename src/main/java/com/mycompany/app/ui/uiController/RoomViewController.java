fb24279f31b9f32959d5c566ca3bf5d74a471f0e
Soney <sonephyo7777777@gmail.com>
2025-05-05 15:50:19 -0400

    update: adding dyanmic card picking and card selection

--- src/main/java/com/mycompany/app/ui/uiController/RoomViewController.java
+++ src/main/java/com/mycompany/app/ui/uiController/RoomViewController.java
@@ -1,5 +1,6 @@
 package com.mycompany.app.ui.uiController;
 
+import com.mycompany.app.ui.utils.CustomUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
@@ -9,8 +10,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Control;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import jdk.jshell.execution.Util;
 
 import java.util.Random;
 
@@ -22,39 +25,52 @@ public class RoomViewController {
     Text username;
     @FXML
     Group userCardsGroup;
+    @FXML
+    ImageView drawPile;
+    @FXML
+    ImageView discardPile;
 
     @FXML
-    Button test;
+    private void addNewCard() {
+        try {
+            System.out.println("at least it is clicked");
+            ImageView imageView = new ImageView();
+            Image image = new Image(CustomUtils.generateRandomCard());
+            imageView.setImage(image);
+            imageView.setFitWidth(67);
+            imageView.setFitHeight(117);
+            imageView.setOnMouseClicked(event -> {
+                discardCard(event, imageView);
+            });
+            imageView.setOnMouseEntered(event -> {
+                imageView.setLayoutY(-20);
+            });
+            imageView.setOnMouseExited(event -> {
+                imageView.setLayoutY(0);
+            });
+            imageView.setPreserveRatio(true);
 
-    @FXML
-    private void addNewCard(ActionEvent event1) {
+            userCardsGroup.getChildren().add(imageView);
+            organizeUserCards();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-        ImageView imageView = new ImageView();
-        Image image = new Image("/cardImages/A_heart2.png");
-        imageView.setImage(image);
-        imageView.setFitWidth(67);
-        imageView.setFitHeight(117);
-        imageView.setId("Testing" + new Random().nextInt(100000));
-        imageView.setOnMouseClicked(event -> {
-            System.out.println(imageView.getId());
-        });
-        imageView.setOnMouseEntered(event -> {
-            imageView.setLayoutY(-20);
-        });
-        imageView.setOnMouseExited(event -> {
-            imageView.setLayoutY(0);
-        });
-        imageView.setPreserveRatio(true);
+    }
 
-        userCardsGroup.getChildren().add(imageView);
+    private void discardCard(MouseEvent event, ImageView imageView) {
+        discardPile.setImage(imageView.getImage());
+        userCardsGroup.getChildren().remove(imageView);
+        organizeUserCards();
+    }
 
-        double layoutDistance = 300.0/userCardsGroup.getChildren().size()+1;
+    private void organizeUserCards() {
+        double layoutDistance = 300.0 / userCardsGroup.getChildren().size() + 1;
         double currentlayOutDistance = layoutDistance;
         for (Node node : userCardsGroup.getChildren()) {
             node.setLayoutX(currentlayOutDistance);
             currentlayOutDistance += layoutDistance;
         }
-
     }
 
     public void setRoomCode(String roomCodeId) {
