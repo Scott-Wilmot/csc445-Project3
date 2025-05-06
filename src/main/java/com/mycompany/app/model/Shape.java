5abc96083ad23ac184ea01245b6d68d22497f16a
Soney <sonephyo7777777@gmail.com>
2025-05-06 09:01:41 -0400

    update: changing enum type

--- src/main/java/com/mycompany/app/model/Shape.java
+++ src/main/java/com/mycompany/app/model/Shape.java
@@ -1,12 +1,18 @@
 package com.mycompany.app.model;
 
 public enum Shape {
-    STAR,
-    MOON,
-    SUN,
-    HEART,
-    SKIP,
-    REVERSE,
-    DRAW_TWO,
-    WILD
+    STAR("st"),
+    MOON("m"),
+    SUN("su"),
+    HEART("h"),
+    SKIP("sk"),
+    REVERSE("r"),
+    DRAW_TWO("p2"),
+    WILD("w");
+
+    private final String shapeValue;
+
+    Shape(String h) {
+        this.shapeValue = h;
+    }
 }
--- src/main/java/com/mycompany/app/model/Value.java
+++ src/main/java/com/mycompany/app/model/Value.java
@@ -3,18 +3,18 @@ package com.mycompany.app.model;
 import java.awt.*;
 
 public enum Value {
-    PAPRIKA(new Color(158, 1, 66)),
-    VALENCIA(new Color(213, 62, 79)),
-    JAFFA(new Color(244, 109, 67)),
-    KOROMIKO(new Color(253, 174, 97)),
-    SALOMIE(new Color(254, 224, 139)),
-    BUTTERFLY_BUSH(new Color(94, 79, 162)),
-    PARADISO(new Color(50, 136, 141)),
-    TRADEWIND(new Color(102, 194, 165)),
-    MOSS_GREEN(new Color(171, 221, 164)),
-    SANDWISP(new Color(230, 245, 152)),
-    ;
+    A("a"),
+    B("b"),
+    C("c"),
+    D("d"),
+    E("e"),
+    F("f"),
+    G("g"),
+    H("h"),
+    I("i"),
+    J("j"),
+    W("w");
 
-    Value(Color color) {
+    Value(String a) {
     }
 }
